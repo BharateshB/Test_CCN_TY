2316ddc5784f561325a1484bdd0d538f9583f437
BharateshB <101256986+BharateshB@users.noreply.github.com>
2024-03-12 23:44:51 +0530

    Update git_dummy_file.java

--- tyss.automation/src/test/java/com/tyss/purplle/scripts/git_dummy_file.java
+++ tyss.automation/src/test/java/com/tyss/purplle/scripts/git_dummy_file.java
@@ -3,6 +3,6 @@ package com.tyss.purplle.scripts;
 public class git_dummy_file {
 
 	public static void main(String[] args) {
-//		this is a git dummy file
+//		this is a git dummy file and u should c in branch
 	}
 }
